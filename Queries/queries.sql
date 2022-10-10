81dcafca11f8d0bf12f04fca9b1faf68010fa43c
DeepikaKulal959 <113009374+DeepikaKulal959@users.noreply.github.com>
2022-10-10 11:05:52 +0530

    Update queries.sql

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -5,41 +5,43 @@ SSL connection (protocol: TLSv1.3, cipher: TLS_AES_256_GCM_SHA384, bits: 256, co
 You are now connected to database "deepika" as user "deepika".
 
 
-deepika=> CREATE TABLE entries (guestName VARCHAR(255), content VARCHAR(255),
-                        entryID SERIAL PRIMARY KEY);
-INSERT INTO entries (guestName, content) values ('first guest', 'I got here!');
-INSERT INTO entries (guestName, content) values ('second guest', 'Me too!');
-CREATE TABLE
-INSERT 0 1
+deepika=>create table Employee(
+        id serial primary key,
+        name varchar(30) not null,
+    age int not null
+ );
+ 
+INSERT INTO Employee (name, age) values ('anu', 27);
 INSERT 0 1
 
+deepika=> INSERT INTO Employee (name, age) values ('raj', 25);
+INSERT 0 1
 
-deepika=> SELECT * FROM entries;
-  guestname   |   content   | entryid
---------------+-------------+---------
- first guest  | I got here! |       1
- second guest | Me too!     |       2
+deepika=>  select * from Employee;
+ id | name | age
+----+------+-----
+  1 | anu  |  27
+  2 | raj  |  25
 (2 rows)
 
 
-deepika=> UPDATE entries SET guestName='manasa',content='hello' WHERE entryid=1;
+
+deepika=> UPDATE Employee SET name='anuradha',age=23 WHERE id=1;
 UPDATE 1
 
-deepika=> SELECT * FROM entries;
-  guestname   | content | entryid
---------------+---------+---------
- second guest | Me too! |       2
- manasa       | hello   |       1
+deepika=>  select * from Employee;
+ id |   name   | age
+----+----------+-----
+  2 | raj      |  25
+  1 | anuradha |  23
 (2 rows)
 
 
-deepika=> DELETE from entries WHERE entryid=2;
+deepika=> DELETE from Employee WHERE id=2;
 DELETE 1
-
-deepika=> SELECT * FROM entries;                                                                                                                                                                                                        
- guestname | content | entryid
------------+---------+---------
- manasa    | hello   |       1
+deepika=>  select * from Employee;
+ id |   name   | age
+----+----------+-----
+  1 | anuradha |  23
 (1 row)
-
 
